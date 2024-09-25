d037d3fbe9ca4e03cad0d90ba124880286e9cca1
Jake <buffso@naver.com>
2024-09-25 22:47:40 +0900

    chore: insert mock data for Startup, Category, and MockInvestor

---
+++ prisma/seed.js
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import { CATEGORIES, STARTUPS, MOCK_INVESTORS } from './mock.js';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.mockInvestor.deleteMany();
+  await prisma.startup.deleteMany();
+  await prisma.category.deleteMany();
+
+  await prisma.category.createMany({
+    data: CATEGORIES,
+    skipDuplicates: true,
+  });
+
+  await prisma.startup.createMany({
+    data: STARTUPS,
+    skipDuplicates: true,
+  });
+
+  await prisma.mockInvestor.createMany({
+    data: MOCK_INVESTORS,
+    skipDuplicates: true,
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
