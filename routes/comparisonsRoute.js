357d51bdf3180fa2ed31cfbab5233e2af229f1bb
unknown <d653@naver.com>
2024-09-26 16:07:40 +0900

    Feat: post-api

--- routes/comparisonsRoute.js
+++ routes/comparisonsRoute.js
@@ -4,9 +4,9 @@ import { PrismaClient } from "@prisma/client";
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// 비교할 기업 검색 API
+// 스타트업 목록 조회 API (검색, 페이지네이션)
 router.get(
-  "/my-startups",
+  "/startup-list",
   asyncHandler(async (req, res) => {
     const { search = "", page = 1, limit = 10 } = req.query;
     const skip = (page - 1) * limit;
@@ -32,26 +32,74 @@ router.get(
       currentPage: parseInt(page),
       totalPages: Math.ceil(totalCount / limit),
       totalCount,
-      startups,
+      list: startups,
     });
   }),
 );
 
-// 최근 선택된 기업 조회 API
-router.get("/my-startups/recently-selected", async (req, res) => {
-  const selectedStartup = await prisma.startup.findMany({
-    where: {
-      count: {
-        gt: 0,
+// 최근 선택된 스타트업 조회 API
+router.get(
+  "/recent",
+  asyncHandler(async (req, res) => {
+    const selectedStartup = await prisma.startup.findMany({
+      where: {
+        count: {
+          gt: 0,
+        },
       },
-    },
-    orderBy: {
-      selectedAt: "desc",
-    },
-    take: 5,
-  });
+      orderBy: {
+        selectedAt: "desc",
+      },
+      take: 5,
+    });
 
-  res.send(selectedStartup);
-});
+    res.send(selectedStartup);
+  }),
+);
+
+// 나의 스타트업 선택 API
+router.post(
+  "/my-startups/select",
+  asyncHandler(async (req, res) => {
+    const { id } = req.body;
+    const startups = await prisma.startup.update({
+      where: { id },
+      data: {
+        count: {
+          increment: 1,
+        },
+      },
+    });
+    res.send(startups);
+  }),
+);
+
+// 비교할 스타트업 선택 API
+router.post(
+  "/comparison-startups/select",
+  asyncHandler(async (req, res) => {
+    const { id } = req.body;
+
+    if (id.length < 1 || id > 5) {
+      return res
+        .status(400)
+        .send({ message: "1개에서 5개 사이의 스타트업을 골라야 합니다." });
+    }
+
+    const updateStartups = await Promise.all(
+      id.map(async (selectedId) => {
+        return await prisma.startup.update({
+          where: { id: selectedId },
+          data: {
+            count: {
+              increment: 1,
+            },
+          },
+        });
+      }),
+    );
+    res.send(updateStartups);
+  }),
+);
 
 export default router;
