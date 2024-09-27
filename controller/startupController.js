fdb1f68e25c7f24381b831d823b4ad7a216cb79e
Jake <buffso@naver.com>
2024-09-27 09:45:31 +0900

    feat: implement fetching a startup's info and its investors

--- controller/startupController.js
+++ controller/startupController.js
@@ -42,3 +42,42 @@ export const getStartups = async (req, res) => {
     totalCount: totalCount
   });
 };
+
+export const getStartupById = async (req, res) => {
+  const { page = 1, limit = 10, order = 'investAmount', sort = 'desc', keyword = '' } = req.query;
+
+  const offset = (page - 1) * limit;
+  const skip = offset;
+  const take = parseInt(limit);
+
+  const { id } = req.params;
+
+  if(isNaN(id)) {
+    return res.status(400).send({ message: 'Invalid ID format. ID must be a number' });
+  }
+
+  const numId = parseInt(id);
+  const startup = await prisma.startup.findUnique({
+    where: { id: numId },
+    include: {
+      mockInvestors: {
+        orderBy:  {[order]: sort },
+        skip,
+        take,
+      },
+    },
+  });
+  if (!startup) {
+    return res.status(404).send({message: 'No startup found with given ID'});
+  }
+  // 전체 투자자수 계산
+  const mockInvestorsCount = await prisma.mockInvestor.count({
+    where: { startupId: numId },
+  });
+  res.send({
+    startup: {
+      ...startup,
+      mockInvestorsCount,    
+    },
+  });
+}
--- routes/startupsRoute.js
+++ routes/startupsRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import asyncHandler from '../middleware/asyncHandler.js';
-import { getStartups } from '../controller/startupController.js'
+import { getStartupById, getStartups } from '../controller/startupController.js'
 
 const router = express.Router();
 
 router.get('/', asyncHandler(getStartups));
+router.get('/:id', asyncHandler(getStartupById));
 
 export default router;
