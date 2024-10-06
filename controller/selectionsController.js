7642ad078898ca874ef2de9b6e7489833a977671
unknown <d653@naver.com>
2024-10-06 20:16:52 +0900

    Fix: method 이름 변경 및 필요없는 api 제거

--- controller/selectionsController.js
+++ controller/selectionsController.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 // 나의 스타트업 선택 API
-export const patchMyStartups = async (req, res) => {
+export const postMyStartups = async (req, res) => {
   const { id, sessionId } = req.body;
 
   const [updateStartup, existingSelection] = await Promise.all([
@@ -44,7 +44,7 @@ export const patchMyStartups = async (req, res) => {
 };
 
 // 비교할 스타트업 여러개 선택 API
-export const patchComparisonStartups = async (req, res) => {
+export const postComparisonStartups = async (req, res) => {
   const { ids, sessionId } = req.body;
 
   if (!Array.isArray(ids) || ids.length < 1 || ids.length > 5) {
@@ -102,110 +102,4 @@ export const patchComparisonStartups = async (req, res) => {
   );
 
   res.send({ updateStartups, newOrUpdateComparisons });
-};
-
-// 나의 기업 선택 취소 API
-export const patchMyUnselect = async (req, res) => {
-  const { id, sessionId } = req.body;
-
-  const startup = await prisma.startup.findUnique({
-    where: { id },
-  });
-
-  if (!startup) {
-    return res.status(404).send({ message: "기업을 찾을 수 없습니다." });
-  }
-
-  const selection = await prisma.selection.findFirst({
-    where: {
-      sessionId: sessionId,
-      startupId: id,
-      isSelected: true,
-    },
-  });
-
-  const updateSelection = await prisma.selection.update({
-    where: {
-      id: selection.id,
-    },
-    data: {
-      isSelected: false,
-      updatedAt: new Date(),
-    },
-  });
-
-  res.send(updateSelection);
-};
-
-// 비교할 기업 선태 취소 API
-export const patchCompareUnselect = async (req, res) => {
-  const { id, sessionId } = req.body;
-
-  const startup = await prisma.startup.findUnique({
-    where: { id },
-  });
-
-  if (!startup) {
-    return res.status(404).send({ message: "기업을 찾을 수 없습니다." });
-  }
-
-  const comparison = await prisma.comparison.findFirst({
-    where: {
-      sessionId: sessionId,
-      startupId: id,
-      isCompared: true,
-    },
-  });
-
-  const updateComparsion = await prisma.comparison.update({
-    where: {
-      id: comparison.id,
-    },
-    data: {
-      isCompared: false,
-      updatedAt: new Date(),
-    },
-  });
-
-  res.send(updateComparsion);
-};
-
-// 전체 선택 취소 및 초기화 API
-export const patchReset = async (req, res) => {
-  const { sessionId } = req.body;
-
-  await Promise.all([
-    prisma.selection.updateMany({
-      where: { sessionId, isSelected: true },
-      data: { isSelected: false },
-    }),
-    prisma.comparison.updateMany({
-      where: { sessionId, isCompared: true },
-      data: { isCompared: false },
-    }),
-  ]);
-
-  const [updatedSelections, updatedComparisons] = await Promise.all([
-    prisma.selection.findMany({
-      where: { sessionId },
-      select: {
-        startupId: true,
-        isSelected: true,
-        updatedAt: true,
-      },
-    }),
-    prisma.comparison.findMany({
-      where: { sessionId },
-      select: {
-        startupId: true,
-        isCompared: true,
-        updatedAt: true,
-      },
-    }),
-  ]);
-
-  res.send({
-    updatedSelections,
-    updatedComparisons,
-  });
 };
