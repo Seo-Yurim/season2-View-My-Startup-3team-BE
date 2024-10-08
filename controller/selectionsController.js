bccb8110f97642d73c935c8f69a318aafca6ddb9
unknown <d653@naver.com>
2024-10-08 19:53:08 +0900

    Feat: 비교 및 선택 취소 api

--- controller/selectionsController.js
+++ controller/selectionsController.js
@@ -103,3 +103,105 @@ export const postComparisonStartups = async (req, res) => {
 
   res.send({ updateStartups, newOrUpdateComparisons });
 };
+
+// 나의 스타트업 선택 취소 API
+export const postCancelMyStartups = async (req, res) => {
+  const { id, sessionId } = req.body;
+
+  const [updateStartup, existingSelection] = await Promise.all([
+    prisma.startup.update({
+      where: { id },
+      data: {
+        selectedCount: {
+          decrement: 1,
+        },
+      },
+    }),
+    prisma.selection.findFirst({
+      where: {
+        sessionId,
+        startupId: id,
+      },
+    }),
+  ]);
+
+  const newOrUpdateSelection = existingSelection
+    ? await prisma.selection.update({
+        where: { id: existingSelection.id },
+        data: {
+          isSelected: false,
+          updatedAt: new Date(),
+        },
+      })
+    : await prisma.selection.create({
+        data: {
+          sessionId,
+          startupId: id,
+          isSelected: false,
+          createdAt: new Date(),
+        },
+      });
+
+  res.send({ updateStartup, newOrUpdateSelection });
+};
+
+// 비교할 스타트업 여러개 선택 취소 API
+export const postCancelComparisonStartups = async (req, res) => {
+  const { ids, sessionId } = req.body;
+
+  if (!Array.isArray(ids) || ids.length < 1 || ids.length > 5) {
+    return res
+      .status(400)
+      .send({ message: "1개에서 5개 사이의 스타트업을 골라야 합니다." });
+  }
+  const [updateStartups, existingComparisons] = await Promise.all([
+    Promise.all(
+      ids.map((id) =>
+        prisma.startup.update({
+          where: { id },
+          data: {
+            comparedCount: {
+              decrement: 1,
+            },
+          },
+        }),
+      ),
+    ),
+
+    Promise.all(
+      ids.map((id) =>
+        prisma.comparison.findFirst({
+          where: {
+            sessionId: sessionId,
+            startupId: id,
+          },
+        }),
+      ),
+    ),
+  ]);
+
+  const newOrUpdateComparisons = await Promise.all(
+    existingComparisons.map((comparison, index) => {
+      if (comparison) {
+        return prisma.comparison.update({
+          where: { id: comparison.id },
+          data: {
+            isCompared: false,
+            updatedAt: new Date(),
+          },
+        });
+      } else {
+        return prisma.comparison.create({
+          data: {
+            sessionId: sessionId,
+            startupId: ids[index],
+            isCompared: false,
+            createdAt: new Date(),
+          },
+        });
+      }
+    }),
+  );
+
+  res.send({ updateStartups, newOrUpdateComparisons });
+};
